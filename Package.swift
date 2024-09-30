312782aa67c0ab34c7697b245ce21ea29c5622b1
Pavel Sharanda <psharanda@gmail.com>
2024-09-30 11:51:58 +0100

    Camera Kit test

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.4
 
 import PackageDescription
 
-let packageName = "Superduper"
-
 let package = Package(
-    name: "Superduper",
-    platforms: [.iOS(.v17)],
-    products: [.library(name: "Superduper", targets: ["Superduper"])],
+    name: "SCCameraKit",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(name: "SCSDKCameraKit", targets: ["SCSDKCameraKit"])
+    ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "Superduper",
-            url: "https://github.com/psharanda/cuddly-octo-dollop/releases/download/2.0.0/Superduper.xcframework.zip",
-            checksum: "3fd042f3c58305b5827b8f1b0d490829ba40a87578524f33f43242fa499022af"
+            name: "SCSDKCameraKit",
+            url: "https://github.com/psharanda/cuddly-octo-dollop/releases/download/1.33.0/SCSDKCameraKit.xcframework.zip",
+            checksum: "61374aee91d2e2f858e1a05a487bfe0af5c8a4da2da8b006114005134e795111"
         )
     ]
 )
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cuddly-octo-dollop
+# test-camera-kit-ios-sdk
